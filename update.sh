84a5f6c874c012f861cb20960342fab9e6cc80f4
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-04-05 18:21:28 -0300

    update the shell script

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+<<note
+
+This script was projected for termux on android
+for use it at your OS, change the path of the alias named path
+
+Thanks for visit this repository!
+note
+
 alias path="~/../usr/bin/win.py"
 rm -rf path
 cp win.py path
