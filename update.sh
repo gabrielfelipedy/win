8d8667857dd4e960fe36d97622775b0828173c60
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-04-15 02:14:45 -0300

    update update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,23 @@ for use it at your OS, change the path of the alias named path
 Thanks for visit this repository!
 note
 
-alias path="~/../usr/bin/win.py"
-rm -rf path
-cp win.py path
+cd ~/../usr/bin/
+echo ------
+echo BEFORE
+echo ------
+ls | tail -n 50
+
+rm -rf win.py
+echo ------
+echo DELETED
+echo ------
+ls | tail -n 50
+
+cd ~/win/
+cp win.py ~/../usr/bin/
+cd ~/../usr/bin/
+echo ---
+echo COPY
+echo ---
+ls | tail -n 50
 echo "[*] Win.py updated"
